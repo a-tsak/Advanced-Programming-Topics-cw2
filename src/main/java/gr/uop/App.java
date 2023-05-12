79c4b30acc3ca0fb5522467a8f3dd951992386cc
Savaskos <stosavas@hotmail.com>
2023-05-13 00:30:13 +0300

    fixed the up and down buttons

--- src/main/java/gr/uop/App.java
+++ src/main/java/gr/uop/App.java
@@ -14,6 +14,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -51,6 +53,7 @@ public class App extends Application {
         filter.setPromptText("type to filter");
         // The left list
         ListView leftList = new ListView<>(itemsLeft);
+        
 
         // First VBox properties
         VBox list1 = new VBox(5);
@@ -65,6 +68,8 @@ public class App extends Application {
         buttons1.setAlignment(Pos.CENTER);
         Button rightButton = new Button();
         Button leftButton = new Button();
+        rightButton.setDisable(true);
+        leftButton.setDisable(true);
         rightButton.setGraphic(right);
         leftButton.setGraphic(left);
 
@@ -86,12 +91,24 @@ public class App extends Application {
         buttons2.setAlignment(Pos.CENTER);
         Button upButton = new Button();
         Button downButton = new Button();
+        upButton.setDisable(true);
+        downButton.setDisable(true);
         upButton.setGraphic(up);
         downButton.setGraphic(down);
 
         buttons2.getChildren().addAll(upButton, downButton);
 
         // Functionalities
+
+
+
+
+
+        leftList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);    //Setting the selection mode for single item
+        rightList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+
+
+
         filter.textProperty().addListener(new ChangeListener<String>() { // Filter application
 
             @Override
@@ -115,6 +132,10 @@ public class App extends Application {
                 leftList.getItems().remove(temp);
                 rightList.getItems().add(temp);
             }
+            buttonCheck(rightList, upButton, downButton);
+
+
+            
 
         });
 
@@ -138,13 +159,109 @@ public class App extends Application {
 
                 leftList.getItems().sort(new CustomComparator());       //Whenever an item is added to the left list, it sorts it by comparing the last digits of the string
             }
+            buttonCheck(rightList, upButton, downButton);
+
+        });
+
+        upButton.setOnAction((e)->{
+            Object temp = rightList.getSelectionModel().getSelectedItem();
+            int currIndex, newIndex;
+
+            currIndex = rightList.getSelectionModel().getSelectedIndex();
+            newIndex = currIndex - 1;
+
+
+            rightList.getItems().remove(temp);
+            rightList.getItems().add(newIndex,temp);
+            rightList.getSelectionModel().select(newIndex);
+            
+
+            buttonCheck(rightList, upButton, downButton);
+
+        });
+
+
+        downButton.setOnAction((e)->{
+            Object temp = rightList.getSelectionModel().getSelectedItem();
+            int currIndex, newIndex;
+
+            currIndex = rightList.getSelectionModel().getSelectedIndex();
+            newIndex = currIndex + 1;
+
+
+            rightList.getItems().remove(temp);
+            rightList.getItems().add(newIndex,temp);
+            rightList.getSelectionModel().select(newIndex);
+
+
+            buttonCheck(rightList, upButton, downButton);
 
         });
 
 
 
-       
 
+
+
+
+        leftList.setOnMouseClicked((e)->{       //enables the right button once you choose something from the list
+            if(leftList.getSelectionModel().getSelectedItem()!=null){
+                rightButton.setDisable(false);
+            }
+        });
+
+        rightList.setOnMouseClicked((e)->{       //enables the left button once you choose something from the list and checks the position of the selected item
+            if(rightList.getSelectionModel().getSelectedItem()!=null){
+                leftButton.setDisable(false);
+
+
+                if(rightList.getSelectionModel().getSelectedIndex()==0){
+                    upButton.setDisable(true);
+                    downButton.setDisable(false);
+                }
+                else if(rightList.getSelectionModel().getSelectedIndex()==rightList.getItems().size()-1){
+                    upButton.setDisable(false);
+                    downButton.setDisable(true);
+                }
+                else{
+                    upButton.setDisable(false);
+                    downButton.setDisable(false);
+                }
+
+
+
+
+
+            }
+            
+
+        });
+        
+       
+        leftList.getItems().addListener(new ListChangeListener<ObservableList>() {
+
+            @Override
+            public void onChanged(Change<? extends ObservableList> c) {
+                if(leftList.getItems().isEmpty()){
+                    rightButton.setDisable(true);
+                }
+            }
+            
+        });
+
+
+        rightList.getItems().addListener(new ListChangeListener<ObservableList>() {
+
+            @Override
+            public void onChanged(Change<? extends ObservableList> c) {
+                if(rightList.getItems().isEmpty()){
+                    leftButton.setDisable(true);
+                    upButton.setDisable(true);
+                    downButton.setDisable(true);
+                }
+            }
+            
+        });
 
 
 
@@ -183,5 +300,40 @@ public class App extends Application {
 
         return filteredList;
     }
+
+
+
+
+    public void buttonCheck(ListView rightList, Button upButton, Button downButton){        //A method that disables and enables buttons 
+
+        if(rightList.getItems().size()<2){
+            upButton.setDisable(true);
+            downButton.setDisable(true);
+            return;
+        }
+
+
+
+        if(rightList.getSelectionModel().getSelectedIndex()==0){
+            upButton.setDisable(true);
+            downButton.setDisable(false);
+        }
+        else if(rightList.getSelectionModel().getSelectedIndex()==rightList.getItems().size()-1){
+            upButton.setDisable(false);
+            downButton.setDisable(true);
+        }
+        else if(rightList.getSelectionModel().getSelectedItem()!=null){
+            upButton.setDisable(false);
+            downButton.setDisable(false);
+        }
+        return;
+    }
+
+
+
+
+
+
+
 
 }
